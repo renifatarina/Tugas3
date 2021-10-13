3d140b91dd7e09208439b7d78992d0621ae1a6de
renifatarina <70559647+renifatarina@users.noreply.github.com>
2021-10-13 17:51:30 +0700

    Add files via upload

---
+++ AplikasiLatihanTiga3/AplikasiLatihanTiga3/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace AplikasiLatihanTiga3
+{
+    partial class frmLatihanTiga3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.lblNama = new System.Windows.Forms.Label();
+            this.cmbPendidikan = new System.Windows.Forms.ComboBox();
+            this.lblPendidikan = new System.Windows.Forms.Label();
+            this.lstPekerjaan = new System.Windows.Forms.ListBox();
+            this.lblPekerjaan = new System.Windows.Forms.Label();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.tblLayout1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tblLayout1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(154, 3);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(475, 22);
+            this.txtNama.TabIndex = 8;
+            this.txtNama.TextChanged += new System.EventHandler(this.txtNama_TextChanged);
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(3, 0);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(61, 17);
+            this.lblNama.TabIndex = 7;
+            this.lblNama.Text = "Nama    ";
+            // 
+            // cmbPendidikan
+            // 
+            this.cmbPendidikan.FormattingEnabled = true;
+            this.cmbPendidikan.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "Pascasarjana"});
+            this.cmbPendidikan.Location = new System.Drawing.Point(154, 41);
+            this.cmbPendidikan.Name = "cmbPendidikan";
+            this.cmbPendidikan.Size = new System.Drawing.Size(475, 24);
+            this.cmbPendidikan.TabIndex = 10;
+            // 
+            // lblPendidikan
+            // 
+            this.lblPendidikan.AutoSize = true;
+            this.lblPendidikan.Location = new System.Drawing.Point(3, 38);
+            this.lblPendidikan.Name = "lblPendidikan";
+            this.lblPendidikan.Size = new System.Drawing.Size(78, 17);
+            this.lblPendidikan.TabIndex = 9;
+            this.lblPendidikan.Text = "Pendidikan";
+            // 
+            // lstPekerjaan
+            // 
+            this.lstPekerjaan.FormattingEnabled = true;
+            this.lstPekerjaan.ItemHeight = 16;
+            this.lstPekerjaan.Items.AddRange(new object[] {
+            "Pedagang",
+            "PNS",
+            "Dosen",
+            "Swasta",
+            "ABRI"});
+            this.lstPekerjaan.Location = new System.Drawing.Point(154, 88);
+            this.lstPekerjaan.Name = "lstPekerjaan";
+            this.lstPekerjaan.Size = new System.Drawing.Size(475, 84);
+            this.lstPekerjaan.TabIndex = 3;
+            // 
+            // lblPekerjaan
+            // 
+            this.lblPekerjaan.AutoSize = true;
+            this.lblPekerjaan.Location = new System.Drawing.Point(3, 85);
+            this.lblPekerjaan.Name = "lblPekerjaan";
+            this.lblPekerjaan.Size = new System.Drawing.Size(76, 17);
+            this.lblPekerjaan.TabIndex = 0;
+            this.lblPekerjaan.Text = "Pekerjaan ";
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.Enabled = false;
+            this.txtPesan2.Location = new System.Drawing.Point(3, 32);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(629, 22);
+            this.txtPesan2.TabIndex = 4;
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.Enabled = false;
+            this.txtPesan1.Location = new System.Drawing.Point(3, 3);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(629, 22);
+            this.txtPesan1.TabIndex = 4;
+            this.txtPesan1.TextChanged += new System.EventHandler(this.txtPesan1_TextChanged);
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.Enabled = false;
+            this.txtPesan3.Location = new System.Drawing.Point(3, 65);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(629, 22);
+            this.txtPesan3.TabIndex = 4;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(3, 95);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(629, 32);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // tblLayout1
+            // 
+            this.tblLayout1.ColumnCount = 1;
+            this.tblLayout1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tblLayout1.Controls.Add(this.txtPesan1, 0, 0);
+            this.tblLayout1.Controls.Add(this.txtPesan2, 0, 1);
+            this.tblLayout1.Controls.Add(this.txtPesan3, 0, 2);
+            this.tblLayout1.Controls.Add(this.btnOk, 0, 3);
+            this.tblLayout1.Location = new System.Drawing.Point(49, 215);
+            this.tblLayout1.Name = "tblLayout1";
+            this.tblLayout1.RowCount = 4;
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblLayout1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tblLayout1.Size = new System.Drawing.Size(635, 140);
+            this.tblLayout1.TabIndex = 11;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.05063F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.94936F));
+            this.tableLayoutPanel1.Controls.Add(this.lstPekerjaan, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblPekerjaan, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cmbPendidikan, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPendidikan, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtNama, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblNama, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(52, 20);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44.70588F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 55.29412F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 106F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(632, 192);
+            this.tableLayoutPanel1.TabIndex = 12;
+            // 
+            // frmLatihanTiga3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 401);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.tblLayout1);
+            this.Name = "frmLatihanTiga3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tugas 3.2 (Desain Menggunakan Komponen Layout)";
+            this.tblLayout1.ResumeLayout(false);
+            this.tblLayout1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label lblPendidikan;
+        private System.Windows.Forms.ComboBox cmbPendidikan;
+        private System.Windows.Forms.Label lblPekerjaan;
+        private System.Windows.Forms.ListBox lstPekerjaan;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan3;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.TableLayoutPanel tblLayout1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
